9c9cd3aa3cad006226c333d864e313dd03f85730
oberon-uofa <alexanderecooper@arizona.edu>
2023-11-16 08:10:37 -0700

    revert

--- src/ModernCCompiler/VirtualMachine/Instructions/IInstruction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VirtualMachine.Instructions
-{
-    public interface IInstruction
-    {
-        void Execute(List<int> memory, Dictionary<string, int> registers, List<string> labels);
-
-        string ToCode();
-    }
-}
